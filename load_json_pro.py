67b556c55f335e4e9bace0fa8ec24d8f712c178a
liuche20083736 <39118657+liuche20083736@users.noreply.github.com>
2020-08-07 10:59:29 +0800

    Update load_json_pro.py
    1. remove the label contains ‘_’ will cause label incomplete.
    2. add the label id print to names.txt

--- load_json_pro.py
+++ load_json_pro.py
@@ -39,7 +39,7 @@ def main(args):
 
                 f_width,f_height = round(float(width/v),6),round(float(height/u),6)
 
-                label_id = str(id_list[data['shapes'][i]['label'].split('_')[0]])
+                label_id = str(id_list[data['shapes'][i]['label']])
 
                 save_yolo_file(label_id,str(center_x),str(center_y),str(f_width),str(f_height),args.data_dir,json_path)
 
@@ -50,11 +50,13 @@ def main(args):
 
         b_name = ['png','jpg']
         
-        dirs_path = os.path.join(os.path.abspath(os.path.join(args.data_dir, "../..")),'yolo_need')
+        dirs_path = os.path.join(os.path.abspath(os.path.join(args.data_dir, "../")),'yolo_data/images/train')
+        dirs_path_val = os.path.join(os.path.abspath(os.path.join(args.data_dir, "../")), 'yolo_data/images/val')
         if os.path.exists(dirs_path):
             pass
         else: 
-            os.mkdir(dirs_path)
+            os.makedirs(dirs_path)
+            os.makedirs(dirs_path_val)
 
         
         if not os.path.exists(os.path.join(args.data_dir,file.split('.')[0]+'.'+'json')):
@@ -67,7 +69,10 @@ def main(args):
              shutil.copyfile(os.path.join(args.data_dir,file),os.path.join(dirs_path,file))
              
     get_train_or_val(args.data_dir)
-    print("save done!") 
+    get_label_id(args.data_dir,id_list)
+    print("save done!")
+
+
 def make_id_list(src_path):
     json_list=[]
     id_list=[]
@@ -79,7 +84,7 @@ def make_id_list(src_path):
     for json_path in json_list:
         data = json.load(open(json_path))
         for i in range(len(data['shapes'])):
-            label = data['shapes'][i]['label'].split('_')[0]
+            label = data['shapes'][i]['label']
             id_list.append(label)
     id_list=list(set(id_list))
 
@@ -90,12 +95,14 @@ def make_id_list(src_path):
     return id_dict
 
 def save_yolo_file(id_name,x,y,w,h,path,json_path):
-    dir_path = os.path.join(os.path.abspath(os.path.join(path,"../..")),'yolo_need')
+    dir_path = os.path.join(os.path.abspath(os.path.join(path,"../")),'yolo_data/labels/train/')
+    dir_path_val = os.path.join(os.path.abspath(os.path.join(path, "../")), 'yolo_data/labels/val/')
     #pdb.set_trace()
     if os.path.exists(dir_path):
         pass
     else:
-        os.mkdir(dir_path)
+        os.makedirs(dir_path)
+        os.makedirs(dir_path_val)
 
     txt_path = os.path.join(dir_path,os.path.basename(json_path).split('.')[0]+'.txt')
 
@@ -105,20 +112,29 @@ def save_yolo_file(id_name,x,y,w,h,path,json_path):
     return 0
 
 def get_train_or_val(path):
-    Txt_path = os.path.join(os.path.abspath(os.path.join(path, "../.."))+'yolo_train.txt')
+    Txt_path = os.path.join(os.path.abspath(os.path.join(path, "../"))+'/yolo_train.txt')
     for file_name in os.listdir(path):
         if file_name.split('.')[-1]=='jpg' or file_name.split('.')[-1]=='png':
             img_path = os.path.join(path,file_name)
             with open(Txt_path,'a+') as f:
                 f.write(img_path+'\n')
 
+
+def get_label_id(path,id_list):
+    Txt_path = os.path.join(os.path.abspath(os.path.join(path, "../"))+'/names.txt')
+    with open(Txt_path,'w') as f:
+        atu = sorted(id_list.items(), key=lambda x: x[1], reverse=False )
+        for ind in range(len(atu)):
+            alist = list(atu[ind])
+            print(alist[0])
+            f.write(alist[0] + '\n')
         
         
 
 def parseArguments(argv):
     parser = argparse.ArgumentParser()
     parser.add_argument('--data_dir',type=str,
-                        help='the path of the json file',default=None)
+                        help='the path of the json file',default='labelme_images')
     return parser.parse_args(argv)
             
     
